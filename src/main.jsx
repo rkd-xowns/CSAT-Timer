4925d8f47ddc1cbee19712bdc14f984bf102152e
rkd-xowns <benjamin.kang070406@gmail.com>
2025-08-12 22:52:20 +0900

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+// 1. ReactDOM을 'react-dom/client'에서 import 합니다.
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-// 1. App.jsx에서 AppWrapper를 기본(default)으로 가져옵니다.
+// App.jsx에서 AppWrapper를 기본(default)으로 가져옵니다.
 import AppWrapper from './App.jsx'; 
-// 2. 기존에 사용하시던 ThemeProvider도 가져옵니다.
+// 기존에 사용하시던 ThemeProvider도 가져옵니다.
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import './index.css';
 
-// 3. AppWrapper를 ThemeProvider와 BrowserRouter로 감싸줍니다.
+// AppWrapper를 ThemeProvider와 BrowserRouter로 감싸줍니다.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
